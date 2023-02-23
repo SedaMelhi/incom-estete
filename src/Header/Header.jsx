c1f1cb0e6e4a2e20ef353a6976e3f1c9b326b7a5
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-02-23 17:19:42 +0300

    закончила header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,28 @@
+import Nav from './Nav/Nav';
+import { useTranslation } from 'react-i18next';
+import './header.sass'
+
+const Header = () => {
+    const { t, i18n } = useTranslation();
+    return(
+        <header>
+            <div className="header">
+                <Nav/>
+                <div className="header__wrap">
+                    <div className="wrap">
+                        <div className="header__text">
+                            <div className="header__title">
+                                {t("header-title")}
+                            </div>
+                            <div className="header__description">
+                                {t("header-desc")}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="nav-size"></div>
+            </div>
+        </header>
+    )
+}
+export default Header;
---
+++ src/Header/Nav/Nav.jsx
@@ -0,0 +1,61 @@
+import {useEffect, useState} from 'react';
+import { useTranslation } from 'react-i18next';
+import './nav.sass';
+
+const Nav = () => {
+    const { t, i18n } = useTranslation();
+    const activeLang = localStorage.getItem("lang") ? localStorage.getItem("lang"): "En";
+    const [lang, setLang] = useState(activeLang)
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    };
+    useEffect(() =>{
+        changeLanguage(lang.toLowerCase())
+    }, [lang])
+    return(
+        <nav>
+            <div className="wrap">
+                <div className="nav nav-size">
+                    <div className="nav__left">
+                        <div className="logo">
+                            <img src="./img/header/logo.svg" alt="INCOM" />
+                        </div>
+                        <div className="menu">
+                            <a href="#" className="menu__item">{t("home")}</a>
+                            <a href="#" className="menu__item">{t("realty")}</a>
+                            <a href="#" className="menu__item">{t("Services")}</a>
+                            <a href="#" className="menu__item">{t("About")}</a>
+                            <a href="#" className="menu__item">{t("Contacts")}</a>
+                        </div>
+                    </div>
+                    <div className="nav__right">
+                        <div className="phone">
+                            <a href="tel:+79289876543" className="phone__number">+ 7 (928) 987 - 65 - 43</a>
+                        </div>
+                        <div className="lang">
+                            <div className="lang__active">{lang}</div>
+                            {/* <div className="long__ru">Ru</div> */}
+                            <div className="lang__wrap">
+                                <div className="lang__arrow">
+                                    <img src="./img/header/arrow.svg" alt=""/>
+                                </div>
+                                <div className="lang-menu__wrap">
+                                    <div className="lang-menu">
+                                        <div className="lang__item" onClick={() => {
+                                            setLang('En')
+                                        }}>EN</div>
+                                        <div className="lang__item" onClick={() => {
+                                            setLang('Ru')
+                                        }}>RU</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
